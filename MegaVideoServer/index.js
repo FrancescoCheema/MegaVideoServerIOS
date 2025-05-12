97f4f7ea35a9b148c64134e3c5e9ee3e1b20535f
FrancescoCheema <cekkosingh@gmail.com>
2025-05-11 17:55:57 -0700

    modified start.sh copy command

--- MegaVideoServer/index.js
+++ MegaVideoServer/index.js
@@ -1,18 +1,14 @@
 const express = require("express");
-const exec = require("child_process");
+const { exec } = require("child_process");
 
 const app = express();
 app.use(express.json());
 
 app.set('port', process.env.PORT || 8080);
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
-})
-
 app.listen(8080, () => {
     console.log("Listening on localhost:8080")
-})
+}) 
 
 app.post('/login', async(req, res) => {
     try{
