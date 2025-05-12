6348bfa232930190a49e69f6f8d68800891ee73e
FrancescoCheema <cekkosingh@gmail.com>
2025-05-11 20:24:55 -0700

    modified docker file and node.js file

--- MegaVideoServer/index.js
+++ MegaVideoServer/index.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 const express = require("express");
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
 
 const app = express();
 app.use(express.json());
@@ -7,18 +8,28 @@ app.use(express.json());
 app.set('port', process.env.PORT || 8080);
 
 app.listen(8080, () => {
-    console.log("Listening on localhost:8080")
+    console.log("Listening on localhost:8080");
+
+    const email = process.env.MEGA_EMAIL;
+    const password = process.env.MEGA_PASSWORD;
+
+    if(!email || !password) {
+        console.error("Missing environment variables MEGA_EMAIL or MEGA_PASSWORD");
+        process.exit(1);
+    }else{
+        const mega = spawn("mega-cmd");
+
+        mega.stdin.write(`login ${email} ${password}`);
+
+        mega.stdout.on("data", (data) =>{
+            console.error(`MEGA STDOUT: ${data}`);
+        })
+
+        mega.stderr.on("data", (data) => {
+            console.error(`MEGA STDERR: ${data}`)
+        })
+    }
 }) 
 
-app.post('/login', async(req, res) => {
-    try{
-        const {email, password} = req.body;
-        const cmd = `mega-login ${email} ${password}`;
-    }
-    catch (error) {
-        console.error(error);
-        req.status(500).json({message: "Internal server error"});
-    }
-})
 
 
